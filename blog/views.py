e845f48e376426ac65d2bdbb31093b3699c0854e
Samy Job Jaimes Medina <sjaimesm@everis.com>
2018-10-10 12:12:36 +0200

    Reglas pep8 corregidas (expeto imports

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,19 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts}) #renderizará (construirá) nuestra plantilla blog/post_list.html
+    # renderizará (construirá) nuestra plantilla blog/post_list.html
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post':post}) 
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -26,6 +30,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,6 +43,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
